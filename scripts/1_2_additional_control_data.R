8fe1a8d03d61342ad9757606161e83440722943c
benediktt98 <benedikt,teuber@edu.rhur-uni-bochum.de>
2026-01-28 11:15:57 +0100

    finalized final district control dataset

--- scripts/1_2_additional_control_data.R
+++ scripts/1_2_additional_control_data.R
@@ -2,16 +2,17 @@ library(sf)
 library(dplyr)
 library(tidyr)
 
-# Pfad zu deinem Ordner "regionaldaten"
+
+#### set working-directory
 
 getwd()
 setwd("C:/Users/bened/OneDrive/Desktop/Uni/Master Economic Policy Consulting/Wintersemester 2025-26/Data Analysis/regionaldaten")
 
-# import data on grid-cells
+##### import data on grid-cells
 
 grid_df <- st_read("grid.geojson")
 
-### import data on POI's for different "Regierungsbezirke" in NRW
+#### import data on POI's for different "Regierungsbezirke" in NRW
 
 railways_arnsberg    <- st_read(file.path( "arnsberg","gis_osm_pois_free_1.shp.shp"), quiet = TRUE)
 pois_detmold     <- st_read(file.path( "detmold",   "gis_osm_pois_free_1.shp"), quiet = TRUE)
@@ -19,7 +20,7 @@ pois_duesseldorf <- st_read(file.path("duesseldorf", "gis_osm_pois_free_1.shp"),
 pois_koeln       <- st_read(file.path( "koeln",       "gis_osm_pois_free_1.shp"), quiet = TRUE)
 pois_muenster    <- st_read(file.path( "muenster",    "gis_osm_pois_free_1.shp"), quiet = TRUE)
 
-# CRS 
+##### set CRS 
 pois_arnsberg    <- st_transform(pois_arnsberg,    25832) %>% mutate(bezirk = "arnsberg")
 pois_detmold     <- st_transform(pois_detmold,     25832) %>% mutate(bezirk = "detmold")
 pois_duesseldorf <- st_transform(pois_duesseldorf, 25832) %>% mutate(bezirk = "duesseldorf")
@@ -27,15 +28,15 @@ pois_koeln       <- st_transform(pois_koeln,       25832) %>% mutate(bezirk = "k
 pois_muenster    <- st_transform(pois_muenster,    25832) %>% mutate(bezirk = "muenster")
 
 
-# bind datasets 
+##### bind regional-datasets into one NRW-dataset
 
 pois_nrw <- bind_rows(
   pois_arnsberg, pois_detmold, pois_duesseldorf, pois_koeln, pois_muenster
 )
 
 
-# filter for relevant POI's
-
+##### filter for relevant POI's
+# to keep it simlpe, we have chosen a few central district characteristics to control for their presence/absence in each grid-cell
 
 pois_nrw_relevant <- pois_nrw %>%
   filter(
@@ -47,7 +48,6 @@ pois_nrw_relevant <- pois_nrw %>%
       "park"
       )
   )
-
 
 
 #### match data with grid-cells
@@ -64,7 +64,7 @@ grid_utm <- st_transform(grid_df, 25832) %>% st_make_valid()
 pois_utm <- st_transform(pois_nrw_relevant, 25832)
 
 
-# join pois-dataset with grid-ids
+##### join pois-dataset with grid-ids
 
 pois_in_grid <- st_join(
   pois_utm,
@@ -77,16 +77,21 @@ pois_in_grid <- st_join(
 ## create dummy-variables for controls for each POI
 # 1 if POI is present in grid-cell, 0 otherwise
 
-wanted <- c("doctors", "hospital", "park", "pharmacy", "supermarket") # define relvant POI's
-
-grid_dummies <- pois_in_grid %>% # code dummies in pois-dataset 
+grid__district_dummies <- pois_in_grid %>%
   st_drop_geometry() %>%
-  filter(fclass %in% wanted) %>%
-  distinct(grid_id, fclass) %>%        
+  distinct(grid_id, fclass) %>%       
   mutate(value = 1L) %>%
   pivot_wider(names_from = fclass, values_from = value, values_fill = 0L)
 
 
-grid_with_dummies_utm <- grid_utm %>% # join them with grid-ids
-  left_join(grid_dummies, by = "grid_id") %>%
+# join those district dummies back with the full grid-cell dataset and code missings to zero
+
+grid_full_with_district_dummies <- grid_utm %>%
+  left_join(grid__district_dummies, by = "grid_id") %>%
   mutate(across(any_of(wanted), ~coalesce(.x, 0L)))
+
+
+#### join district dummies with final analysis dataset
+
+
+
