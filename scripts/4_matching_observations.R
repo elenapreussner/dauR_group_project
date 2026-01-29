ae112e0286bc9a8539997d6136ec479ddd8da800
benediktt98 <benedikt,teuber@edu.rhur-uni-bochum.de>
2026-01-29 17:28:44 +0100

    matching completed and conducted regression analysis for the mathed dataset

--- scripts/4_matching_observations.R
+++ scripts/4_matching_observations.R
@@ -20,10 +20,13 @@ library(knitr)
 
 
 
-# conduct balance check
+##### conduct balance check
 
 balance_check <- matchit(
-  school_nearby ~ living_area + rooms_n + age_building + immigrants_percents + average_age,
+  school_nearby ~ living_area +  site_area + 
+    rooms_n + baths_n + age_building +  cellar +
+    immigrants_percents +  average_age +  pharmacy +  supermarket +
+    hospital + doctors +  park,
   data = full_dataset_main_clean,
   method = NULL
 )
@@ -33,9 +36,36 @@ balance_check <- matchit(
 summary(balance_check)
 
 
-### applying the rule of thumb that all standard mean differences are >= 0.1, we assume that the CIA is not violated
-## therefore, matching on observables is not necessary and we continue our anaylsis with the current data-set
+### applying the rule of thumb that all standard mean differences are >= 0.1, we assume that the CIA is not violated for the most controls
+## for some, standard mean differences exceed the thumb rule
+## CIA is violated
 
 
+#### conduct matching on observable covariates
 
+# estimate propensity score
+
+ps_baseline <- matchit(
+  school_nearby ~ living_area +  site_area + 
+    rooms_n + baths_n + age_building +  cellar +
+    immigrants_percents +  average_age +  pharmacy +  supermarket +
+    hospital + doctors +  park,
+  data = full_dataset_main_clean,
+  method = "nearest",
+  distance = "logit",
+  ratio = 1         
+)
+
+
+# inspection 
+
+summary(ps_baseline)
+
+# exctract matched dataset
+
+matched_data <- match.data(ps_baseline)
+
+
+matched_data %>%
+  count(school_nearby)
 
--- scripts/5_regression_analysis.R
+++ scripts/5_regression_analysis.R
@@ -153,6 +153,87 @@ modelsummary(
 
 
 
+###############################################
+####### second variant for matched data #######
+###############################################
+
+
+# only treatment indicator
+m1_matched <- feols(
+  log(price_sqm) ~ school_nearby | gid2019,
+  data = matched_data,
+  vcov = "HC3"
+)
+
+
+
+# treatment indicator + housing characteristics
+
+m2_matched <- feols(
+  log(price_sqm) ~ school_nearby + living_area +  site_area + 
+    rooms_n + baths_n + age_building +  I(age_building^2) | gid2019,
+  data = matched_data,
+  vcov = "HC3"
+)
+
+
+
+# treatment indicator + housing and neighborhood characteristics
+
+m3_matched <- feols(
+  log(price_sqm) ~ school_nearby + living_area +  site_area + 
+    rooms_n + baths_n + age_building +  I(age_building^2) +  cellar +
+    immigrants_percents +  average_age +  pharmacy +  supermarket +
+    hospital + doctors +  park| gid2019,
+  data  = matched_data,
+  vcov = "HC3"
+)
+
+
+## create model summary for main specification
+
+modelsummary(
+  list(
+    "Baseline" = m1_matched,
+    "Model 2"  = m2_matched,
+    "Model 3"  = m3_matched
+  ),
+  coef_map = c(
+    "school_nearby"        = "School nearby",
+    "living_area"          = "living area",
+    "site_area"            = "Site area",
+    "rooms_n"              = "Number of rooms",
+    "baths_n"              = "Number of bathrooms",
+    "age_building"         = "Building age",
+    "I(age_building^2)"    = "Building age squared",
+    "cellar"               = "Cellar",
+    "immigrants_percents"   = "Immigrants (%)",
+    "average_age"          = "Average age",
+    "pharmacy"             = "Pharmacy nearby",
+    "supermaket"           = "Supermarket nearby",
+    "hospital"             = "Hospital nearby",
+    "doctors"              = "Doctor nearby",
+    "park"                 = "Park nearby"
+  ),
+  add_rows = data.frame(
+    term = "Region fixed effects",
+    Baseline = "✔",
+    `Model 2` = "✔",
+    `Model 3` = "✔",
+    check.names = FALSE
+  ),
+  gof_map = data.frame(
+    raw   = c("nobs", "r.squared"),
+    clean = c("Observations", "R²"),
+    fmt   = c(0, 3)
+  ),
+  title = "Effect of secondary School Proximity on House Prices",
+  stars = TRUE,
+  gof_omit = "IC|Log|Adj",
+  notes = "Note: Robust standard errors (HC3) in parentheses."
+)
+
+
 
 
 
